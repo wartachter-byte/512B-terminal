380849eb44d4fd7a51272b8d1cfe8931e0762535
wartachter-byte <wartachter@gmail.com>
2026-02-24 16:05:07 +0100

    Forgot what i added.

--- src/bash/bash.asm
+++ src/bash/bash.asm
@@ -7,6 +7,13 @@ start:
 	mov al, '>'    ; Een prompt teken
 	int 0x10       ; Print het
 
+	; the buffer is after our code so on 0x07d00
+	; segment = 0x07dxx >>> 0x07d0
+	; offset = 0, just do xor di, di
+	push 0x07d0
+	pop es
+	xor di, di
+
 ;main loop.
 main:
 	; Get key.
@@ -14,36 +21,36 @@ main:
 	mov ah, 0x00
 	int 0x16
 
-	; Check if the user typed a backspace. for the wrap we need to check it now.
+	
+	; Check if the user typed a backspace
 	cmp al,8
 	je main_backspace
+main_resume_a:
+	; check offset of buffer against 63
+	cmp di, 63
+	; if bigger or eqaul. so beyond buffer or at the end of it, go back to the main and dont print anything
+	jae main
 
+	; store the currently typed byte
+	stosb
+
+main_resume_b:
 	; Print key.
 	; Input in AL which lines uo with the Get Key output.
 	mov ah, 0x0e
 	int 0x10
 
-
+	
 
 	; Jump back for the loop.
 	jmp main
 
 ;handles the backspace.
 main_backspace:
-	; segment location of the BDA colomn and row.
-	; push and pop is needed cause mov es, 0x0040 gives a error.
-	; after that we move colomn in DL and row in DH.
-	push 0x0040      
-	pop es
-	mov dx, [es:0x50]
-		
-	; now we need to see if the colomn (DL) it is zero.
-	; we test against itself cause that also sets the zero flag if it is zero.
-	; if it is zero we jump to the wrap handler.
-	; from the wrap handler we also jump back to the main loop.
-	test dl, dl
-	jz main_backspace_wrap
-
+	; move the buffer pointer back 1
+	dec di
+	; for print
+	mov ah, 0x0e
 	; print the backspace
 	int 0x10
 	
@@ -64,20 +71,8 @@ main_backspace:
 
 	
 	;and now go back to the main loop.
-	jmp main
+	jmp main_resume_b
 
-; handles the wrap for the backspace.
-main_backspace_wrap:
-	; BIOS call set cursor
-	mov ah, 02h
-	; xor is smaller and is okay right now
-	xor bh, bh
-	; decrement the row to go up. we already have the value
-	dec dh
-	; dl needs to be the max value (79)
-	mov dl, 79
-	; call the BIOS
-	int 0x10
 	
 ; the magic code.
 times 510-($-$$) db 0
