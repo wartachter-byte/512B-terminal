2e799d1d6dba6e0033159c1e2c3b25cc5c053ac0
wartachter-byte <wartachter@gmail.com>
2026-02-27 18:02:23 +0100

    Cleaned up the toolchain a bit.

--- make.sh
+++ make.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-python3 create.py
+if [ "$(stat -c %Y create.c)" != "$(cat create.c.timestamp 2>/dev/null)" ]; then
+    stat -c %Y create.c > create.c.timestamp
+    gcc create.c -o create
+fi
+./create
