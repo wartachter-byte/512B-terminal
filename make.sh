1f1339959d13419b59be160524926b849cb3fdfc
wartachter-byte <wartachter@gmail.com>
2026-02-28 09:04:40 +0100

    forgot to commit this

--- make.sh
+++ make.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-if [ "$(stat -c %Y create.c)" != "$(cat create.c.timestamp 2>/dev/null)" ]; then
-    stat -c %Y create.c > create.c.timestamp
-    gcc create.c -o create
+if [ ! -f ./tls/create.c ]; then
+    echo -e "\e[31mERROR: create.c was not found.\e[0m"
+    exit 1
 fi
-./create
+
+if [ "$(stat -c %Y tls/create.c)" != "$(cat tls/create.c.timestamp 2>/dev/null)" ]; then
+    gcc tls/create.c -o tls/create
+    succes=$?
+    
+    echo $succes > tls/create.c.succes
+    stat -c %Y tls/create.c > tls/create.c.timestamp
+else
+    succes=$(cat tls/create.c.succes 2>/dev/null)
+fi
+
+if [ "$succes" = "0" ]; then
+    ./tls/create
+fi
