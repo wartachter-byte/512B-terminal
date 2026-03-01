fdcde3118641db14a8f2363b97f8841a76dc5010
wartachter-byte <wartachter@gmail.com>
2026-03-01 14:43:37 +0100

    Im stopping. My brain hurts.

--- make.sh
+++ make.sh
@@ -5,6 +5,12 @@ if [ ! -f ./tls/create.c ]; then
     exit 1
 fi
 
+if [ ! -f ./src/bash/bash.asm ]; then
+    echo -e "\e[31mERROR: bash.asm was not found.\e[0m"
+    exit 1
+fi
+
+
 if [ "$(stat -c %Y tls/create.c)" != "$(cat tls/create.c.timestamp 2>/dev/null)" ]; then
     gcc tls/create.c -o tls/create
     succes=$?
@@ -15,6 +21,14 @@ else
     succes=$(cat tls/create.c.succes 2>/dev/null)
 fi
 
+if [ "$(stat -c %Y src/bash/bash.asm)" != "$(cat tls/bash.asm.timestamp 2>/dev/null)" ]; then
+    nasm src/bash/bash.asm -o bin/bash.img
+    
+    stat -c %Y src/bash/bash.asm > tls/bash.asm.timestamp
+fi
+
 if [ "$succes" = "0" ]; then
     ./tls/create
+    truncate -s %512 bin/app_header
+    cat bin/app_header >> bin/bash.img
 fi
--- src/bash/bash.asm
+++ src/bash/bash.asm
@@ -1,8 +1,19 @@
 org 0x7c00
 bits 16
 
+%define SectorsPerTrack 18
+%define NumberOfHeads 2
+
 ; start code for confirmation
 start:
+	; Set the stack pointer to the start of our code beacuse it grows downward
+	mov sp, 0x7c00
+
+	; also this is at the start cause dl wil contain the boot drive ID
+	; and idk what all these BIOS calls after this do to dl
+	; and we modify di later on to
+	push dx
+
 	; clears the screen by resetting the videomode. causes flicker, but its smaller
 	; ah=00h videomode set al=03h, standard videomode
 	mov ax, 0003h
@@ -23,6 +34,8 @@ start:
 	pop es
 	xor di, di
 
+	
+
 ;main loop.
 main:
 	; Get key.
@@ -35,8 +48,9 @@ main:
 	cmp al,8
 	je main_backspace
 
-	
-main_resume_a:
+	cmp al, 13
+	je run
+
 	; check offset of buffer against 63
 	cmp di, 63
 	; if bigger or eqaul. so beyond buffer or at the end of it, go back to the main and dont print anything
@@ -45,7 +59,7 @@ main_resume_a:
 	; store the currently typed byte
 	stosb
 
-main_resume_b:
+main_resume:
 	; Print key.
 	; Input in AL which lines uo with the Get Key output.
 	mov ah, 0x0e
@@ -56,7 +70,127 @@ main_resume_b:
 	; Jump back for the loop.
 	jmp main
 
-;handles the backspace.
+run:
+	; pass
+
+; Loads a sector using LBA
+; in:
+; 	ax - Sector
+;	cl - Count of sectors
+; 	es:bx - Where to put
+; out:
+; 	Putted on memory
+.load:
+	; Using the LBA_to_CHS function, i need to preserve some registers.
+	; Mainly: cx and bx
+	; As these registers are modified
+	; Also we want to modify no primary registers.
+	pusha
+
+	; So now need to calculate.
+	; The BIOS could fail and i don't want to uselessly recalculate.
+	; So we are gonna calculate and then store the result.
+	; except, the calculation modifies a lot.
+	; So il just set it up once and then store it.
+
+	push cx
+	push bx
+	push es
+	call .load_convert
+	pop es
+	pop bx
+	pop ax
+	mov ah, 02h
+	pusha
+	push es
+	mov si, 3
+	
+.load_loop:
+	int 13h
+	jnc .load_succes
+
+	pop es
+	popa
+
+	xor ax, ax
+	int 13h
+
+	dec si
+	jnz .load_loop
+
+	jmp .load_fail
+
+.load_succes:
+	popa
+	ret
+
+.load_fail:
+	popa
+	ret
+	
+; converts LBA to CHS
+; in:
+; 	ax - LBA
+; out:
+; 	ch - low 8 bits of cylinder
+; 	cl =
+;		0-5 - sector
+;		6-7 - upper 2 bits of cylinder
+;	dh - head
+; 	dl - drive number
+; modifications:
+; 	ax, bx, cx, dx
+.load_convert:
+	; Sector   = (LBA % SPT) + 1
+	; Head 	   = (LBA / SPT) % NOH
+	; Cylinder = (LBA / SPT) / NOH
+
+	; Plan:
+	; Calculate (LBA /% SPT), and +1, and /%NOH. lastly move some regs.
+	
+	
+	; clean dx because div uses DX:AX
+	xor dx, dx
+	; set the value in bx for the div
+	mov bx, SectorsPerTrack
+	; divide!
+	div bx
+
+	; Calculate the rest of the remainder/Sector.
+	inc dx
+	; store the remainder/Sector for later.
+	push dx
+
+	; Now for the head and cylinder
+	; clean dx because div uses DX:AX
+	xor dx, dx
+	; set the value in bx for the div
+	mov bx, NumberOfHeads
+	; DIVIDE!!!
+	div bx
+
+	; Now DL has the Head, AL the Cylinder, and the Sector is in the stack.
+	; The Head must be in DH, the Cylinder in CH, and the Sector in CL
+	; Pop's must be done first, because they operate 16-bit
+	
+	; Get the sector. It is in CL now, which is perfect.
+	pop cx
+	; Get the drive number. It now is in BL. Im storing it in BX because the head is in DX
+	pop bx
+	; Also save it again.
+	push bx
+
+	; Now we can do the mov operations.
+	; Move the Head to DH
+	mov dh, dl
+	; Move the drive-number to dl
+	mov dl, bl
+	; Move the Cylinder to CH
+	mov ch, al
+	
+; just handels the buffers
+; modify's di, ah and al
+; di - buffer offset
 main_backspace:
 
 	; is the buffer empty?
@@ -84,9 +218,9 @@ main_backspace:
 
 	
 	;and now go back to the main loop.
-	jmp main_resume_b
+	jmp main_resume
 
-	
+
 ; the magic code.
 times 510-($-$$) db 0
 dw 0xaa55
